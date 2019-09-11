50139edaa5b17d72f96ba51f5a2fa9a8f2c65367
Christopher Nowack Rockwell <cnris@vl-fb.eecs.umich.edu>
2019-09-10 22:18:58 -0400

    coordconv added

---
+++ models/coordconv.py
@@ -0,0 +1,109 @@
+import torch
+from torch import nn
+
+class AddCoordinates(object):
+
+    r"""Coordinate Adder Module as defined in 'An Intriguing Failing of
+    Convolutional Neural Networks and the CoordConv Solution'
+    (https://arxiv.org/pdf/1807.03247.pdf).
+    This module concatenates coordinate information (`x`, `y`, and `r`) with
+    given input tensor.
+    `x` and `y` coordinates are scaled to `[-1, 1]` range where origin is the
+    center. `r` is the Euclidean distance from the center and is scaled to
+    `[0, 1]`.
+    Args:
+        with_r (bool, optional): If `True`, adds radius (`r`) coordinate
+            information to input image. Default: `False`
+    Shape:
+        - Input: `(N, C_{in}, H_{in}, W_{in})`
+        - Output: `(N, (C_{in} + 2) or (C_{in} + 3), H_{in}, W_{in})`
+    Examples:
+        >>> coord_adder = AddCoordinates(True)
+        >>> input = torch.randn(8, 3, 64, 64)
+        >>> output = coord_adder(input)
+        >>> coord_adder = AddCoordinates(True)
+        >>> input = torch.randn(8, 3, 64, 64).cuda()
+        >>> output = coord_adder(input)
+        >>> device = torch.device("cuda:0")
+        >>> coord_adder = AddCoordinates(True)
+        >>> input = torch.randn(8, 3, 64, 64).to(device)
+        >>> output = coord_adder(input)
+    """
+
+    def __init__(self, with_r=False):
+        self.with_r = with_r
+
+    def __call__(self, image):
+        batch_size, _, image_height, image_width = image.size()
+
+        y_coords = 2.0 * torch.arange(image_height).unsqueeze(
+            1).expand(image_height, image_width) / (image_height - 1.0) - 1.0
+        x_coords = 2.0 * torch.arange(image_width).unsqueeze(
+            0).expand(image_height, image_width) / (image_width - 1.0) - 1.0
+
+        coords = torch.stack((y_coords, x_coords), dim=0)
+
+        if self.with_r:
+            rs = ((y_coords ** 2) + (x_coords ** 2)) ** 0.5
+            rs = rs / torch.max(rs)
+            rs = torch.unsqueeze(rs, dim=0)
+            coords = torch.cat((coords, rs), dim=0)
+
+        coords = torch.unsqueeze(coords, dim=0).repeat(batch_size, 1, 1, 1)
+
+        image = torch.cat((coords.to(image.device), image), dim=1)
+
+        return image
+
+class CoordConvTranspose(nn.Module):
+
+    r"""2D Transposed Convolution Module Using Extra Coordinate Information
+    as defined in 'An Intriguing Failing of Convolutional Neural Networks and
+    the CoordConv Solution' (https://arxiv.org/pdf/1807.03247.pdf).
+    Args:
+        Same as `torch.nn.ConvTranspose2d` with two additional arguments
+        with_r (bool, optional): If `True`, adds radius (`r`) coordinate
+            information to input image. Default: `False`
+    Shape:
+        - Input: `(N, C_{in}, H_{in}, W_{in})`
+        - Output: `(N, C_{out}, H_{out}, W_{out})`
+    Examples:
+        >>> coord_conv_tr = CoordConvTranspose(3, 16, 3, with_r=True)
+        >>> input = torch.randn(8, 3, 64, 64)
+        >>> output = coord_conv_tr(input)
+        >>> coord_conv_tr = CoordConvTranspose(3, 16, 3, with_r=True).cuda()
+        >>> input = torch.randn(8, 3, 64, 64).cuda()
+        >>> output = coord_conv_tr(input)
+        >>> device = torch.device("cuda:0")
+        >>> coord_conv_tr = CoordConvTranspose(3, 16, 3, with_r=True).to(device)
+        >>> input = torch.randn(8, 3, 64, 64).to(device)
+        >>> output = coord_conv_tr(input)
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size,
+                 stride=1, padding=0, output_padding=0, groups=1, bn=False, bias=True,
+                 dilation=1, with_r=False):
+        super(CoordConvTranspose, self).__init__()
+
+        in_channels += 2
+        if with_r:
+            in_channels += 1
+
+        self.conv_tr_layer = nn.ConvTranspose2d(in_channels, out_channels,
+                                                kernel_size, stride=stride,
+                                                padding=padding,
+                                                output_padding=output_padding,
+                                                groups=groups, bias=bias,
+                                                dilation=dilation)
+        self.bn = None
+        if bn:
+            self.bn = nn.BatchNorm2d(out_channels)
+        self.coord_adder = AddCoordinates(with_r)
+
+    def forward(self, x):
+        x = self.coord_adder(x)
+        x = self.conv_tr_layer(x)
+        if self.bn is not None:
+            x = self.bn(x)
+            
+        return x
