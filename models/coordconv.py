bd4fa90fe27c487f83c46585385215547235d4ec
Christopher Nowack Rockwell <cnris@vl-fb.eecs.umich.edu>
2019-09-27 23:48:31 -0400

    update for pytorch 1

--- models/coordconv.py
+++ models/coordconv.py
@@ -1,6 +1,61 @@
 import torch
 from torch import nn
 
+class AddCoords(nn.Module):
+
+    def __init__(self, with_r=False):
+        super().__init__()
+        self.with_r = with_r
+
+    def forward(self, input_tensor):
+        """
+        Args:
+            input_tensor: shape(batch, channel, x_dim, y_dim)
+        """
+        batch_size, _, x_dim, y_dim = input_tensor.size()
+
+        xx_channel = torch.arange(x_dim).repeat(1, y_dim, 1)
+        yy_channel = torch.arange(y_dim).repeat(1, x_dim, 1).transpose(1, 2)
+
+        xx_channel = xx_channel.float() / (x_dim - 1)
+        yy_channel = yy_channel.float() / (y_dim - 1)
+
+        xx_channel = xx_channel * 2 - 1
+        yy_channel = yy_channel * 2 - 1
+
+        xx_channel = xx_channel.repeat(batch_size, 1, 1, 1).transpose(2, 3)
+        yy_channel = yy_channel.repeat(batch_size, 1, 1, 1).transpose(2, 3)
+
+        ret = torch.cat([
+            input_tensor,
+            xx_channel.type_as(input_tensor),
+            yy_channel.type_as(input_tensor)], dim=1)
+
+        if self.with_r:
+            rr = torch.sqrt(torch.pow(xx_channel.type_as(input_tensor) - 0.5, 2) + torch.pow(yy_channel.type_as(input_tensor) - 0.5, 2))
+            ret = torch.cat([ret, rr], dim=1)
+
+        return ret
+    
+'''
+class CoordConv(base.Layer):
+    """CoordConv layer as in the paper."""
+    def __init__(self, x_dim, y_dim, with_r, *args,  **kwargs):
+        super(CoordConv, self).__init__()
+        self.addcoords = AddCoords(x_dim=x_dim, 
+                                   y_dim=y_dim, 
+                                   with_r=with_r,
+                                   skiptile=True)
+        self.conv = tf.layers.Conv2D(*args, **kwargs)
+
+    def call(self, input_tensor):
+        ret = self.addcoords(input_tensor)
+        ret = self.conv(ret)
+        return ret
+
+
+
+
 class AddCoordinates(object):
 
     r"""Coordinate Adder Module as defined in 'An Intriguing Failing of
@@ -54,7 +109,7 @@ class AddCoordinates(object):
         image = torch.cat((coords.to(image.device), image), dim=1)
 
         return image
-
+'''
 class CoordConvTranspose(nn.Module):
 
     r"""2D Transposed Convolution Module Using Extra Coordinate Information
@@ -98,10 +153,13 @@ class CoordConvTranspose(nn.Module):
         self.bn = None
         if bn:
             self.bn = nn.BatchNorm2d(out_channels)
-        self.coord_adder = AddCoordinates(with_r)
+        #self.coord_adder = AddCoordinates(with_r)
+        
+        self.addcoords = AddCoords(with_r=with_r)
 
     def forward(self, x):
-        x = self.coord_adder(x)
+        x = self.addcoords(x)
+        #x = self.coord_adder(x)
         x = self.conv_tr_layer(x)
         if self.bn is not None:
             x = self.bn(x)
